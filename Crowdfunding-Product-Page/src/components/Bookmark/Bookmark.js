de98409e94103bae9b24034a267b45c2e2bdfe93
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2021-03-28 18:51:57 +0100

    Minor fixes. Removed dupilcate images.

--- Crowdfunding-Product-Page/src/components/Bookmark/Bookmark.js
+++ Crowdfunding-Product-Page/src/components/Bookmark/Bookmark.js
@@ -5,7 +5,7 @@ export default function Bookmark({ active = false, ...props }) {
 	return (
 		<div className={`bookmark ${active && 'active'}`}>
 			<svg className="bookmark__icon" width="56" height="56" xmlns="http://www.w3.org/2000/svg">
-				<g fill="none" fill-rule="evenodd">
+				<g fill="none" fillRule="evenodd">
 					<circle className="bookmark__icon--circle" cx="28" cy="28" r="28"/>
 					<path className="bookmark__icon--tag" d="M23 19v18l5-5.058L33 37V19z"/>
 				</g>
--- Crowdfunding-Product-Page/src/components/Button/Button.js
+++ Crowdfunding-Product-Page/src/components/Button/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Button({ disabled = false, children, ...props }) {
 	return (
-		<button className={disabled && "disabled"}>
+		<button className={`${disabled && 'disabled'}`}>
 			{children}
 		</button>
 	)
