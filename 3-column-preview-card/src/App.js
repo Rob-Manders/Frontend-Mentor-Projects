41c196912399fb5f2b7e93a7bf14883079612c40
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2021-05-23 16:37:12 +0100

    Component finalised.

--- 3-column-preview-card/src/App.js
+++ 3-column-preview-card/src/App.js
@@ -1,12 +1,42 @@
 
-import {Card} from './components/'
+import React, {useState} from 'react'
+import {Card, Vehicle} from './components/'
 
 export default function App() {
-  return (
-    <>
-      <Card>
-        3 Column Preview Card
-      </Card>
-    </>
-  )
+	const [vehicles, setVehicles] = useState([
+		{
+			id: 0,
+			type: 'sedan',
+			title: 'Sedans',
+			description: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip',
+			colour: 'hsl(31, 77%, 52%)',
+			icon: 'images/icon-sedans.svg'
+		},
+		{
+			id: 1,
+			type: 'suv',
+			title: 'SUVs',
+			description: 'Take an SUV for its spacious interior, power, ans versatility. Perfect for your next family vacation and off-road adventures.',
+			colour: 'hsl(184, 100%, 22%)',
+			icon: 'images/icon-suvs.svg'
+		},
+		{
+			id: 2,
+			type: 'luxury',
+			title: 'Luxury',
+			description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+			colour: 'hsl(179, 100%, 13%)',
+			icon: 'images/icon-luxury.svg'
+		}
+	])
+
+	return (
+		<>
+			<Card>
+				{
+					vehicles.map(vehicle => <Vehicle key={`vehicle_${vehicle.id}`} vehicle={vehicle} />)
+				}
+			</Card>
+		</>
+	)
 }
---
+++ 3-column-preview-card/src/components/vehicle/Vehicle.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import Button from '../button/Button'
+
+export default function Vehicle({vehicle}) {
+	const {
+		type,
+		title,
+		description,
+		colour,
+		icon
+	} = vehicle
+
+	return (
+		<div className={`vehicle ${type}`}>
+			<img src={icon}></img>
+			<h2>{title}</h2>
+			<p>{description}</p>
+			<Button href='#'>Learn More</Button>
+		</div>
+	)
+}
